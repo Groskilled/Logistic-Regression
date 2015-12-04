141e4e84e1eb0cf70c2a29d795dd16d00dc3b811
Groskilled <adam.wybierala@gmail.com>
2015-12-04 19:05:27 +0000

    Debugging...

--- Python/logisticRegression.py
+++ Python/logisticRegression.py
@@ -4,15 +4,22 @@ import csv
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+import scipy.optimize as opt
+from scipy.optimize import fmin
 
-def sigmoid(Theta, X):
-    ret = []
-    tmp = np.dot(X, Theta)
-    for i in range(X.shape[0]):
-        ret.append([1.0 / (1.0 + math.exp(-tmp[i]))])
-    return (np.array(ret))
+def sigmoid(X):
+    return (1 / (1 + np.exp(-X)))
 
-def gradientDescent(X, Y):
+def cost(Theta, X, Y):
+    tmp = sigmoid(X * Theta)
+    return (-Y * np.log(tmp) - (1 - Y) * np.log(1 - tmp)).mean()
+
+def gradient(Theta, X, Y):
+    hyp = sigmoid(X * Theta) - Y
+    grad = Theta
+    for i in range(X.shape[1]):
+      grad[i] = (hyp * X[:,i].reshape(len(Y), 1).T).mean()
+    return grad
 
 def main():
     cr = csv.reader(open("../Data.csv","rb"))
@@ -20,10 +27,12 @@ def main():
     Y = []
     for row in cr:
         X.append([float(row[0]), float(row[1])])
-        Y.append(float(row[2]))
-    X = np.insert(np.array(X, dtype=float), 0, 1.0, axis=1)
-    Theta = np.array([[0.0], [0.0], [0.0]])
-    print(sigmoid(Theta, X))
+        Y.append([float(row[2])])
+    X = np.matrix(X)
+    Y = np.matrix(Y)
+    Theta = np.matrix(np.zeros(X.shape[1]))
+    print(cost(Theta, X, Y))
+    print(gradient(Theta, X, Y))
 
 if __name__ == "__main__":
     main()
