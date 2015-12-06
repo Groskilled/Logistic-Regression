69db5a2141cc5fb74f9d914a2218ee8ad3dd8524
Groskilled <adam.wybierala@gmail.com>
2015-12-06 12:36:57 +0000

    grad and cost ok

--- Python/logisticRegression.py
+++ Python/logisticRegression.py
@@ -11,14 +11,14 @@ def sigmoid(X):
     return (1 / (1 + np.exp(-X)))
 
 def cost(Theta, X, Y):
-    tmp = sigmoid(X * Theta)
+    tmp = sigmoid(np.dot(X, Theta))
     return (-Y * np.log(tmp) - (1 - Y) * np.log(1 - tmp)).mean()
 
 def gradient(Theta, X, Y):
-    hyp = sigmoid(X * Theta) - Y
+    hyp = sigmoid(np.dot(X, Theta)) - Y
     grad = Theta
     for i in range(X.shape[1]):
-      grad[i] = (hyp * X[:,i].reshape(len(Y), 1).T).mean()
+      grad[i] = (hyp * X[:,i].reshape(len(Y), 1)).mean()
     return grad
 
 def main():
@@ -28,9 +28,9 @@ def main():
     for row in cr:
         X.append([float(row[0]), float(row[1])])
         Y.append([float(row[2])])
-    X = np.matrix(X)
-    Y = np.matrix(Y)
-    Theta = np.matrix(np.zeros(X.shape[1]))
+    Y = np.array(Y)
+    X = np.insert(np.array(X), 0, 1.0, axis=1)
+    Theta = np.zeros((X.shape[1], 1))
     print(cost(Theta, X, Y))
     print(gradient(Theta, X, Y))
 
